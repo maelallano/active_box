0e0a9b653a1d29cee3c178a48cdf43f54d92410f
mallano <maelallano1@gmail.com>
2018-12-01 16:00:11 +0100

    Download section done

---
+++ src/components/Download/Download.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import './Download.css';
+
+export default class Download extends Component {
+
+    render() {
+		return (
+            <div className="downContainer">
+                <div className="downloadTitle">
+                    <h3>Are You Ready to Start? Download Now For Free!</h3>
+                </div>
+
+                <div className="downloadSubTitle">
+                    <h4>FUSCE DAPIBUS, TELLUS AC CURSUS COMMODO</h4>
+                </div>
+
+                <div className="downloadBtnBox">
+                    <a className="downloatBtn" href="#">download for free</a>
+                </div>
+            </div>
+		);
+	}
+}
