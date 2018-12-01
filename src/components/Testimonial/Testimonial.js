9c384d5a882cb0b1600001e3b9f9d224224de48c
mallano <maelallano1@gmail.com>
2018-12-01 15:59:47 +0100

    Testimonial section done

---
+++ src/components/Testimonial/Testimonial.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import './Testimonial.css';
+import testimonialImg from '../../img/testimonial-1.jpg';
+
+export default class Testimonial extends Component {
+
+    render() {
+		return (
+            <div className="boxContainer">
+                <img className="boxImg" src={testimonialImg} alt="" />
+                <div className="box">
+                    <h3 className="testimonialTitle">"Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec sed odio dui. Aenean eu leo quam..."</h3>
+                    <p className="testimonialP">SUSAN SIMS, INTERACTION DESIGNER AT XYZ</p>
+                </div>
+            </div>
+		);
+	}
+}
